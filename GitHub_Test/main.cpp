ec6fccc247dd5e5628ee0d838311808975e453fb
lysanderweekes <33299338+lysanderweekes@users.noreply.github.com>
2017-11-01 20:04:13 -0400

    Add files via upload

---
+++ GitHub_Test/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string username;
+    string password;
+
+    cout << "Enter username: ";
+    cin >> username;
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    if ((username == "test") && (password == "123")) {
+        system("CLS");
+        cout << "login successful" << endl;
+    }
+    else {
+        system("CLS");
+        cout << "login unsuccessful, please try again" << endl;
+    }
+
+    return 0;
+}
